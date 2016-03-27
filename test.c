93115dec82735493ab205d1201abbb3cac4116ec
root <root@debian>
2016-03-27 18:11:01 +0300

    first commit

---
+++ test.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <json/json.h>
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <string.h>
+
+int takeExtension(char *file)
+{
+char *extention = malloc(5);	
+int i = 0;
+int j=0;
+while(file[i]!='.')
+{
+	/*
+	if(i==0)
+	{
+		return 0;
+	}
+	else
+	{
+		i--;	
+	}
+	*/
+	i++;
+}
+	while(file[i] !='\0')
+		{
+			printf("%s",file[i]);
+			extention[j]=file[i];
+			i++;
+		}
+		printf("%s", extention);
+	 return 0;
+}
+
+int main(int argc, char *argv[]){
+ 
+  char *filename = malloc(100);
+  char *outputName = malloc(100);
+  strcpy(filename,argv[1]);
+
+if(filename == NULL)
+{
+	printf("%s,Please add xml or json file as an input\n\n");
+	
+}
+ else
+ {
+ 	takeExtension(filename);
+		
+ }
+
+
+ return 0;
+}
+
+
+
+
