050774398024f6488fe43035d9350d94b4e2d8fa
mertcan <mertcan@debian>
2016-03-27 22:58:32 +0300

    last

--- test.c
+++ test.c
@@ -1,59 +1,70 @@
-#include <stdio.h>
-#include <json/json.h>
-#include <libxml/tree.h>
-#include <libxml/parser.h>
-#include <string.h>
+    #include <stdio.h>
+    #include <json/json.h>
+    #include <libxml/tree.h>
+    #include <libxml/parser.h>
+    #include <string.h>
 
-int takeExtension(char *file)
-{
-char *extention = malloc(10);	
-int i = 0;
-int j=0;
-while(file[i]!='.')
-{
-	/*
-	if(i==0)
-	{
-		return 0;
-	}
-	else
-	{
-		i--;	
-	}
-	*/
-	i++;
-}
-	while(file[i] !='\0')
-		{
-			//printf("%c",file[i]);
-			extention[j]=file[i];
-			//printf("%c",extention[i]);
-			i++;
-			j++;
-		}
-		printf("%s\n", extention);
-	 return 0;
-}
+
 
 int main(int argc, char *argv[]){
- 
-  char *filename = malloc(100);
-  char *outputName = malloc(100);
-  strcpy(filename,argv[1]);
 
-if(filename == NULL)
-{
-	printf("%s,Please add xml or json file as an input\n\n");
-	
+    char *filename = malloc(100);
+    char *outputName = malloc(100);
+    strcpy(filename,argv[1]);
+
+    char *extention = malloc(10);	
+    int i = strlen(filename);
+    int j=0;
+
+
+
+    if(filename == NULL)
+    {
+    	printf("%s,Please add xml or json file as an input\n\n");
+    	
+    }
+    else
+    {
+      while(filename[i]!='.')
+      {
+    	   /*
+    	   if(i==0)
+    	   {
+    	     	return 0;
+    	    }
+    	   else
+    	   {
+    		  i--;	
+    	   }
+        	*/
+          i--;
+      }
+      while(filename[i] !='\0')
+      {
+    			//printf("%c",file[i]);
+         extention[j]=filename[i];
+    			//printf("%c",extention[i]);
+         i++;
+         j++;
+     }
+
+
+     if(strcmp(extention,".json")==0)
+     {
+
+              // json to xml
+     }
+     else if(strcmp(extention,".xml")==0){
+
+     }
+     else{
+        printf("%s\n Please enter extention proper\n" );
+
+    }
 }
- else
- {
- 	takeExtension(filename);
-		
- }
 
 
- return 0;
+return 0;
 }
 
 
